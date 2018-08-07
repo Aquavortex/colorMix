9a5a79d8f6b5fe507629982c63f1ab8830a7e2a6
Aquavortex <Aquavortex.yandex.ru>
2018-08-07 19:54:31 +0300

    Add sliders and button reset which is looking like bee, also add border to window

--- colorMix v.2/ViewController.swift
+++ colorMix v.2/ViewController.swift
@@ -15,8 +15,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        colorView.backgroundColor = .black
+        colorView.layer.borderWidth = 5
+        colorView.layer.cornerRadius = 20
+        colorView.layer.borderColor = UIColor.black.cgColor
         updateColor()
+        updateControls()
+        
+        
+      
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,10 +35,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSwitch: UISwitch!
     @IBOutlet weak var greenSwitch: UISwitch!
     @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
     
     @IBAction func switchChanged(_ sender: UISwitch) {
        updateColor()
+        updateControls()
         }
     
     
@@ -42,16 +53,44 @@ class ViewController: UIViewController {
         var blue: CGFloat = 0
         
         if redSwitch.isOn {
-            red = 1
+            red = CGFloat(redSlider.value)
         }
         if greenSwitch.isOn{
-            green = 1
+            green = CGFloat(greenSlider.value)
         }
         if blueSwitch.isOn {
-            blue = 1
+            blue = CGFloat(blueSlider.value)
         }
         let color = UIColor( red: red, green: green, blue: blue, alpha: 1 )
         colorView.backgroundColor = color
     }
+    
+    @IBAction func sliderChange(_ sender: UISlider) {
+        updateColor()
+    }
+    
+    func updateControls() {
+        greenSlider.isEnabled = greenSwitch.isOn
+        redSlider.isEnabled = redSwitch.isOn
+        blueSlider.isEnabled = blueSwitch.isOn
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        redSlider.value = 0
+        greenSlider.value = 0
+        blueSlider.value = 0
+        
+        redSwitch.isOn = false
+        greenSwitch.isOn = false
+        blueSwitch.isOn = false
+        
+        updateColor()
+        updateControls()
+        
+    }
+    
+    
+    
+    
 }
 
